05b5fd98c5792e77395ee18bca51983cf1affea3
Aunabil Chakma <aunabilac@gmail.com>
2026-03-22 23:38:01 -0700

    udpated experiments commands

--- codes/sample_command.sh
+++ codes/sample_command.sh
@@ -79,6 +79,8 @@ nohup python -u core_trainer_cluster.py --model "Qwen/Qwen3-4B" --device-map "cu
 ### taxonomy - dummy
 nohup python -u core_trainer_taxonomy.py --model "Qwen/Qwen3-4B" --device-map "cuda:0" > nohup_outs/nohup_taxonomy_qwen4.out 2>&1 &
 
+
+### Current experiments
 ### etgpo taxonomy RE
 ## base taxonomy 3 runs
 nohup python -u etgpo/etgpo_full.py --task_mode relation_extraction_non_reasoning --dataset fs_tacred --methods taxonomy \
@@ -98,8 +100,56 @@ nohup python -u etgpo/etgpo_full.py --task_mode relation_extraction_non_reasonin
 nohup python -u etgpo/etgpo_full.py --task_mode relation_extraction_non_reasoning --dataset fs_tacred --methods taxonomy_cluster \
 --main_model "Qwen/Qwen3-4B" --taxonomy_model "Qwen/Qwen3-14B" --device_map "cuda:0" --valid_size 1000 --taxonomy_runs 1 \
 --eval_runs 1 --num_guidance_prompts 3 --taxonomy_num_clusters 5 --output_dir "../unified_optimization_results/" --taxonomy_batch_size 6 \
-> nohup_outs/nohup_etgpo_re_taxonomy_cluster_run.out 2>&1 &
+> nohup_outs/nohup_etgpo_re_taxonomy_cluster_run1.out 2>&1 &
 
+nohup python -u etgpo/etgpo_full.py --task_mode relation_extraction_non_reasoning --dataset fs_tacred --methods taxonomy_cluster \
+--main_model "Qwen/Qwen3-4B" --taxonomy_model "Qwen/Qwen3-14B" --device_map "cuda:1" --valid_size 1000 --taxonomy_runs 1 \
+--eval_runs 1 --num_guidance_prompts 3 --taxonomy_num_clusters 5 --output_dir "../unified_optimization_results/" --taxonomy_batch_size 6 \
+> nohup_outs/nohup_etgpo_re_taxonomy_cluster_run2.out 2>&1 &
+
+nohup python -u etgpo/etgpo_full.py --task_mode relation_extraction_non_reasoning --dataset fs_tacred --methods taxonomy_cluster \
+--main_model "Qwen/Qwen3-4B" --taxonomy_model "Qwen/Qwen3-14B" --device_map "cuda:3" --valid_size 1000 --taxonomy_runs 1 \
+--eval_runs 1 --num_guidance_prompts 3 --taxonomy_num_clusters 5 --output_dir "../unified_optimization_results/" --taxonomy_batch_size 6 \
+> nohup_outs/nohup_etgpo_re_taxonomy_cluster_run3.out 2>&1 &
+
+
+## base taxonomy 3 runs with more coverage + guidlines
+
+nohup python -u etgpo/etgpo_full.py --task_mode relation_extraction_non_reasoning --dataset fs_tacred --methods taxonomy \
+--main_model "Qwen/Qwen3-4B" --taxonomy_model "Qwen/Qwen3-14B" --device_map "cuda:3" --valid_size 1000 --taxonomy_runs 1 \
+--eval_runs 1 --num_guidance_prompts 5 --max_guidances 10 --coverage_threshold 0.9 \
+--output_dir "../unified_optimization_results/" --taxonomy_batch_size 6 > nohup_outs/nohup_etgpo_re_taxonomy_mg10_ct_0.9_run1.out 2>&1 &
+
+nohup python -u etgpo/etgpo_full.py --task_mode relation_extraction_non_reasoning --dataset fs_tacred --methods taxonomy \
+--main_model "Qwen/Qwen3-4B" --taxonomy_model "Qwen/Qwen3-14B" --device_map "cuda:0" --valid_size 1000 --taxonomy_runs 1 \
+--eval_runs 1 --num_guidance_prompts 5 --max_guidances 10 --coverage_threshold 0.9 \
+--output_dir "../unified_optimization_results/" --taxonomy_batch_size 6 > nohup_outs/nohup_etgpo_re_taxonomy_mg10_ct_0.9_run2.out 2>&1 &
+
+nohup python -u etgpo/etgpo_full.py --task_mode relation_extraction_non_reasoning --dataset fs_tacred --methods taxonomy \
+--main_model "Qwen/Qwen3-4B" --taxonomy_model "Qwen/Qwen3-14B" --device_map "cuda:1" --valid_size 1000 --taxonomy_runs 1 \
+--eval_runs 1 --num_guidance_prompts 5 --max_guidances 10 --coverage_threshold 0.9 \
+--output_dir "../unified_optimization_results/" --taxonomy_batch_size 6 > nohup_outs/nohup_etgpo_re_taxonomy_mg10_ct_0.9_run3.out 2>&1 &
+
+
+## base taxonomy cluster 3 runs with more coverage + guidlines
+
+nohup python -u etgpo/etgpo_full.py --task_mode relation_extraction_non_reasoning --dataset fs_tacred --methods taxonomy_cluster \
+--main_model "Qwen/Qwen3-4B" --taxonomy_model "Qwen/Qwen3-14B" --device_map "cuda:3" --valid_size 1000 --taxonomy_runs 1 \
+--eval_runs 1 --num_guidance_prompts 5 --max_guidances 10 --coverage_threshold 0.9 \
+--output_dir "../unified_optimization_results/" --taxonomy_batch_size 6 > nohup_outs/nohup_etgpo_re_taxonomy_cluster_mg10_ct_0.9_run1.out 2>&1 &
+
+nohup python -u etgpo/etgpo_full.py --task_mode relation_extraction_non_reasoning --dataset fs_tacred --methods taxonomy_cluster \
+--main_model "Qwen/Qwen3-4B" --taxonomy_model "Qwen/Qwen3-14B" --device_map "cuda:0" --valid_size 1000 --taxonomy_runs 1 \
+--eval_runs 1 --num_guidance_prompts 5 --max_guidances 10 --coverage_threshold 0.9 \
+--output_dir "../unified_optimization_results/" --taxonomy_batch_size 6 > nohup_outs/nohup_etgpo_re_taxonomy_cluster_mg10_ct_0.9_run2.out 2>&1 &
+
+nohup python -u etgpo/etgpo_full.py --task_mode relation_extraction_non_reasoning --dataset fs_tacred --methods taxonomy_cluster \
+--main_model "Qwen/Qwen3-4B" --taxonomy_model "Qwen/Qwen3-14B" --device_map "cuda:1" --valid_size 1000 --taxonomy_runs 1 \
+--eval_runs 1 --num_guidance_prompts 5 --max_guidances 10 --coverage_threshold 0.9 \
+--output_dir "../unified_optimization_results/" --taxonomy_batch_size 6 > nohup_outs/nohup_etgpo_re_taxonomy_cluster_mg10_ct_0.9_run3.out 2>&1 &
+
+
+##################################################
 # tags custom
 --feedback-open-tag "[f]" --feedback-close-tag "[/f]" --prompt-open-tag "[p]" --prompt-close-tag "[/p]"
 
